044def6b06485ff48344ec79aff731c891e881d0
Likesh Singla <Singlalikesh81@gmail.com>
2025-12-02 23:53:28 +0530

    Project upload

--- include/buyer.h
+++ include/buyer.h
@@ -1,5 +1,12 @@
 #ifndef BUYER_H
 #define BUYER_H
 
+extern char buyer_name[50];
+extern char buyer_address[200];
+extern long long buyer_phone;
+extern char receiver_name[50];
+extern char mode_of_payment[20];
+
 void buyer(void);
+
 #endif
