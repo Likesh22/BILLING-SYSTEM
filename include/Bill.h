5c3b408389aa6ff600e0488b7f56e0db4301377b
Likesh Singla <Singlalikesh81@gmail.com>
2025-12-02 21:06:57 +0530

    Project upload

---
+++ include/Bill.h
@@ -0,0 +1,6 @@
+#ifndef Bill_H
+#define Bill_H
+
+void Bill(const char *type);
+
+#endif
--- include/structure.h
+++ include/structure.h
@@ -1,15 +1,20 @@
 #ifndef STRUCT_H
 #define STRUCT_H
 
-struct Items{ 
+typedef struct Items{ 
 	char product_name[50];
 	int quantity;
 	float rate;
 	int no_of_items;
 	float amount;
-};
+}Items;
 
 extern float Fsgst ,Fcgst ,Fgst ,FAmount ,Ftotal_amount;
 extern float Psgst ,Pcgst ,Pgst ,PAmount ,Ptotal_amount;
+
+
+extern struct Items item[100];
+extern int n;
+extern int i;
 
 #endif
